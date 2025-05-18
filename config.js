dd20c4d1818c806c5c4e345747f6c36794d39555
VINIX <148594993+VINIX100@users.noreply.github.com>
2025-05-18 23:12:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PATRON-MD~3bf0157ae0869dfd267876f8a398f5cb",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
